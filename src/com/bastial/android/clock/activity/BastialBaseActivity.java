5c392bc5e365cd77297f560064b9dd8c8c1b4a58
peekaboo- <923373067@qq.com>
2013-04-25 02:01:22 +0800

    init

--- src/com/bastial/android/clock/activity/BastialBaseActivity.java
+++ src/com/bastial/android/clock/activity/BastialBaseActivity.java
@@ -17,7 +17,6 @@ import com.bastial.android.clock.activity.manage.BastialActivityManager;
  * 
  */
 public class BastialBaseActivity extends Activity {
-
 	private final String TAG = "BastialBaseActivity";
 	
 	private BastialActivityManager mActivityManager = null;
